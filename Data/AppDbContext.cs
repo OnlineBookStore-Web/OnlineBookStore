c53b975b9c967015a1f9b204c003ce45becb3843
rewan abdellah <rowanahmed713@gmail.com>
2025-12-07 02:05:47 +0200

    cart model was added

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,7 +15,10 @@ namespace OnlineBookStore.Data
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrdersDetails { get; set; }
         public DbSet<User> Users { get; set; }
+        //rewan part
+        public DbSet<Cart> Carts { get; set; }
 
+   
     }
 }
 
