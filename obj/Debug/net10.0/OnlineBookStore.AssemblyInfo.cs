de23519cfcadaa4082fd3fd42b9ec7c9a78bc644
rewan abdellah <rowanahmed713@gmail.com>
2025-12-09 15:26:21 +0200

    "Add cart controller and views"

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e76e30bdc032087c4318ea93bfb6eb5a0123852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53b975b9c967015a1f9b204c003ce45becb3843")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
