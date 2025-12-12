22c7cf70c42d8a3b304d2862ac694d92460f9973
ShroukMostafa <mostafashrouk707@gmail.com>
2025-12-13 01:59:42 +0200

    ..

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7fb45105ca9d4b3ed2e6f2085e44dfe5d07c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93dd55cb64a737e6de2bdef5dfe2a231536f40d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
