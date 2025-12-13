81a95b3a3447b14a4a580837253ce228768c5bf4
sara omran <so2767288@gmail.com>
2025-12-13 02:00:33 +0200

    ...

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7684ff626ad4077ce96ba309e8d97b7b4fe11c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3648657694c744954f2e25cbbad65222b3bc03a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
