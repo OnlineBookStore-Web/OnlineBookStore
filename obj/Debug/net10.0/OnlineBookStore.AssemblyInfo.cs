36d58b83da7e85ad2b36c5352f20ff8daa122f4a
sara omran <so2767288@gmail.com>
2025-12-19 01:31:43 +0200

    Changed the HomePage

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ec666fb1cdac4796e09eaee591cafe83b6fc81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7befaca98268b685847d6b789587b3d2a7e329b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
