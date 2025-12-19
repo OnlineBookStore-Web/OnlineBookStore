5010a4fb592fac01ed30aacc7abb2c342073a3dd
sara omran <so2767288@gmail.com>
2025-12-19 05:35:13 +0200

    added Cartitem migration

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4728d9729e6b0117de71cb740fa37b7334aacb77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10d46929e573e28f7f67065043d01527710c2c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
