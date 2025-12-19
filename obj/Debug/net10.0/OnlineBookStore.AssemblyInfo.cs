0ff956f25b47cf86d8237a4b5ebeae01fa768ad1
sara omran <so2767288@gmail.com>
2025-12-19 04:18:33 +0200

    trying to fix the checkout

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c541802281577a6d3508a3a48a32ff543249ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a4f37703884f7c0e8d72b9a2fed536250c97ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
