8ed419be78b3e4edf2967cf23c0abe23665a2f21
rewan abdellah <rowanahmed713@gmail.com>
2025-12-19 03:57:24 +0200

    ty

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c541802281577a6d3508a3a48a32ff543249ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbbc314656634dfbc628cb3d4cae4245ca28b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
