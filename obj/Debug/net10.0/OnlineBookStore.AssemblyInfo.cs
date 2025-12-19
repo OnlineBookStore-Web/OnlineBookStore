2aaf5fee58b866136eeb21d0fc26d1466071bc21
rewan abdellah <rowanahmed713@gmail.com>
2025-12-19 05:55:31 +0200

    عايزه ارجعه زى مكان

--- obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a7ed44ddedab09dada46a7df797508648476e92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfde12a4997fc7f4c7f8648dcb38ca9c99be95f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
